511eb13d48733e4a861fc9d5155933ec2a46e8ab
Carlo Andaya <carlo@carloandaya.com>
2012-10-29 18:47:04 -0700

    Create BNRImageStore

---
+++ Homepwner/BNRImageStore.m
@@ -0,0 +1,55 @@
+//
+//  BNRImageStore.m
+//  Homepwner
+//
+//  Created by Carlo Andaya on 10/29/12.
+//  Copyright (c) 2012 Carlo Andaya. All rights reserved.
+//
+
+#import "BNRImageStore.h"
+
+@implementation BNRImageStore
+
++ (id)allocWithZone:(NSZone *)zone
+{
+    return [self sharedStore];
+}
+
++ (BNRImageStore *)sharedStore
+{
+    static BNRImageStore *sharedStore = nil;
+    if (!sharedStore) {
+        // Create the singleton
+        sharedStore = [[super allocWithZone:NULL] init];
+    }
+    return sharedStore;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        dictionary = [[NSMutableDictionary alloc] init];
+    }
+    return self;
+}
+
+- (void)setImage:(UIImage *)i forKey:(NSString *)s
+{
+    [dictionary setObject:i forKey:s];
+}
+
+- (UIImage *)imageForKey:(NSString *)s
+{
+    return [dictionary objectForKey:s];
+}
+
+- (void)deleteImageForKey:(NSString *)s
+{
+    if (!s) { // test for empty string
+        return;
+    }
+    [dictionary removeObjectForKey:s];
+}
+
+@end
