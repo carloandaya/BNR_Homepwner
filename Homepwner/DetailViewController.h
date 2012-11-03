489791495d01ff3773e020bf165628d406e367cc
Carlo Andaya <carlo@carloandaya.com>
2012-11-03 00:47:01 -0700

    Implemented popover controller for iPad.

--- Homepwner/DetailViewController.h
+++ Homepwner/DetailViewController.h
@@ -10,13 +10,15 @@
 
 @class BNRItem;
 
-@interface DetailViewController : UIViewController <UINavigationControllerDelegate, UITextFieldDelegate, UIImagePickerControllerDelegate>
+@interface DetailViewController : UIViewController <UINavigationControllerDelegate, UITextFieldDelegate, UIImagePickerControllerDelegate, UIPopoverControllerDelegate>
 {
     __weak IBOutlet UITextField *nameField;
     __weak IBOutlet UITextField *serialNumberField;
     __weak IBOutlet UITextField *valueField;
     __weak IBOutlet UILabel *dateLabel;
     __weak IBOutlet UIImageView *imageView;
+    
+    UIPopoverController *imagePickerPopover;
 }
 
 - (IBAction)changeDate:(id)sender;
